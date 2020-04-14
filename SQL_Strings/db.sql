d285051dccc693d3fe1a93263772d4ff294c10b4
Martin <mail@mama.sh>
2020-04-14 19:16:56 +0200

    cleaned file

--- SQL_Strings/db.sql
+++ SQL_Strings/db.sql
@@ -1,108 +1,113 @@
-CREATE TABLE `Customer` (
-  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(25) NOT NULL,
-  `last_name` varchar(25) NOT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  `postal_code` varchar(4) DEFAULT NULL,
-  `city` varchar(25) DEFAULT NULL,
-  `phone` varchar(8) NOT NULL,
-  `credit_limit` int(11) NOT NULL,
-  PRIMARY KEY (`customer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*OK*/
+CREATE TABLE Department (
+    DepartmentName varchar(10) NOT NULL,
+    BossID char(6) DEFAULT NULL,
+    FOREIGN KEY BossID REFERENCES Employee(EmployeeID)
+)
 
-CREATE TABLE `Employee` (
-  `EmployeeID` char(6) NOT NULL,
-  `FirstName` varchar(25) NOT NULL,
-  `LastName` varchar(25) NOT NULL,
-  `Address` varchar(50) DEFAULT NULL,
-  `PostalCode` varchar(4) DEFAULT NULL,
-  `City` varchar(25) DEFAULT NULL,
-  `TlfNumber` varchar(8) DEFAULT NULL,
-  `Salary` int(11) DEFAULT NULL,
-  `Title` varchar(25) DEFAULT NULL,
-  `Department` varchar(25) DEFAULT NULL,
-  `start_date` date DEFAULT NULL,
-  `end_date` date DEFAULT NULL,
-  PRIMARY KEY (`EmployeeID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE Customer (
+    CustomerID char(7) NOT NULL,
+    FirstName varchar(25) NOT NULL,
+    LastName varchar(25) NOT NULL,
+    Address varchar(50) DEFAULT NULL,
+    PostalCode char(4) DEFAULT NULL,
+    City varchar(25) DEFAULT NULL,
+    Phone varchar(8) NOT NULL,
+    CreditLimit INT NOT NULL,
+    PRIMARY KEY (customer_id)
+) 
 
-CREATE TABLE `Product` (
-  `ProductID` char(6) NOT NULL,
-  `Details` varchar(50) DEFAULT NULL,
-  `HTMLDescription` varchar(50) DEFAULT NULL,
-  `Image` varchar(25) DEFAULT NULL,
-  `SupplierID` char(6) DEFAULT NULL,
-  `PurchasePrice` int(30) NOT NULL,
-  `SalesPrice` int(30) NOT NULL,
-  `StockQuantity` int(10) DEFAULT NULL,
-  `TransportSC` varchar(30) DEFAULT NULL,
-  `TransportCC` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`ProductID`),
-  KEY `SupplierID` (`SupplierID`),
-  CONSTRAINT `Product_ibfk_1` FOREIGN KEY (`SupplierID`) REFERENCES `Supplier` (`SupplierID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE Employee (
+    EmployeeID char(6),
+    FirstName varchar(25) NOT NULL,
+    LastName varchar(25) NOT NULL,
+    Address varchar(50) DEFAULT NULL,
+    PostalCode varchar(4) DEFAULT NULL,
+    City varchar(25) DEFAULT NULL,
+    Phone varchar(8) DEFAULT NULL,
+    Salary INT DEFAULT NULL,
+    Title varchar(25) DEFAULT NULL,
+    Department varchar(10) NOT NULL,
+    StartDate date DEFAULT NULL,
+    EndDate date,
+    FOREIGN KEY (Department) REFERENCES Department(DepartmentName)
+    PRIMARY KEY (EmployeeID, end_date)
+) 
 
-CREATE TABLE `PurchaseOrder` (
-  `PurchaseOrderID` char(6) NOT NULL,
-  `SupplierID` char(6) NOT NULL,
-  `OrderDate` date NOT NULL,
-  `DateOfReceipt` date DEFAULT NULL,
-  `DateOfPayment` date DEFAULT NULL,
-  PRIMARY KEY (`PurchaseOrderID`),
-  KEY `SupplierID` (`SupplierID`),
-  CONSTRAINT `PurchaseOrder_ibfk_1` FOREIGN KEY (`SupplierID`) REFERENCES `Supplier` (`SupplierID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE Product (
+    ProductID char(6) NOT NULL,
+    Details varchar(200) DEFAULT NULL,
+    HTMLDescription varchar(200) DEFAULT NULL,
+    Image varchar(25) DEFAULT NULL,
+    SupplierID char(6) DEFAULT NULL,
+    PurchasePrice INT NOT NULL,
+    SalesPrice INT NOT NULL,
+    StockQuantity INT DEFAULT NULL,
+    TransportSC varchar(30) DEFAULT NULL,
+    TransportCC varchar(30) DEFAULT NULL,
+    PRIMARY KEY (ProductID),
+    FOREIGN KEY (SupplierID) REFERENCES Supplier (SupplierID)
+) 
 
-CREATE TABLE `PurchaseOrderLine` (
-  `PurchaseOrderID` char(6) NOT NULL,
-  `ProductID` char(6) NOT NULL,
-  `Amount` int(10) DEFAULT NULL,
-  `PurchasePrice` int(30) DEFAULT NULL,
-  KEY `PurchaseOrderID` (`PurchaseOrderID`),
-  KEY `ProductID` (`ProductID`),
-  CONSTRAINT `PurchaseOrderLine_ibfk_1` FOREIGN KEY (`PurchaseOrderID`) REFERENCES `PurchaseOrder` (`PurchaseOrderID`),
-  CONSTRAINT `PurchaseOrderLine_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `Product` (`ProductID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE PurchaseOrder (
+  PurchaseOrderID char(6) NOT NULL,
+  SupplierID char(6) NOT NULL,
+  OrderDate date NOT NULL,
+  DateOfReceipt date DEFAULT NULL,
+  DateOfPayment date DEFAULT NULL,
+  PRIMARY KEY (PurchaseOrderID),
+  FOREIGN KEY (SupplierID) REFERENCES Supplier (SupplierID)
+) 
 
-CREATE TABLE `SalesOrder` (
-  `SalesOrderID` char(7) NOT NULL,
-  `customer_id` int(11) NOT NULL,
-  `ShippingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `BillingDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `PaymentDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`SalesOrderID`),
-  KEY `customer_id` (`customer_id`),
-  CONSTRAINT `SalesOrder_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `Customer` (`customer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE PurchaseOrderLine (
+  PurchaseOrderID char(6) NOT NULL,
+  ProductID char(6) NOT NULL,
+  Amount INT DEFAULT NULL,
+  PurchasePrice DECIMAL(20,2) DEFAULT NULL,
+  FOREIGN KEY (PurchaseOrderID) REFERENCES PurchaseOrder (PurchaseOrderID),
+  FOREIGN KEY (ProductID) REFERENCES Product (ProductID)
+) 
 
-CREATE TABLE `SalesOrderLine` (
-  `SalesOrderID` char(7) NOT NULL,
-  `ProductID` char(6) NOT NULL,
-  `Amount` int(10) DEFAULT NULL,
-  `SalesPrice` int(30) DEFAULT NULL,
-  KEY `SalesOrderID` (`SalesOrderID`),
-  KEY `ProductID` (`ProductID`),
-  CONSTRAINT `SalesOrderLine_ibfk_1` FOREIGN KEY (`SalesOrderID`) REFERENCES `SalesOrder` (`SalesOrderID`),
-  CONSTRAINT `SalesOrderLine_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `Product` (`ProductID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE SalesOrder (
+  SalesOrderID char(7) NOT NULL,
+  CustomerID INT NOT NULL,
+  OrderDate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  ShippingDate timestamp DEFAULT NULL
+  InvoiceDate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PaymentDate timestamp DEFAULT NULL
+  PRIMARY KEY (SalesOrderID),
+  FOREIGN KEY (customer_id) REFERENCES Customer (customer_id)
+) 
 
-CREATE TABLE `Supplier` (
-  `SupplierID` char(6) NOT NULL,
-  `SupplierName` varchar(25) DEFAULT NULL,
-  `ContactPerson` varchar(25) DEFAULT NULL,
-  `Address` varchar(50) DEFAULT NULL,
-  `PostalCode` varchar(4) DEFAULT NULL,
-  `City` varchar(25) DEFAULT NULL,
-  `TlfNumber` varchar(8) DEFAULT NULL,
-  PRIMARY KEY (`SupplierID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE SalesOrderLine (
+    SalesOrderID char(7) NOT NULL,
+    ProductID char(6) NOT NULL,
+    Amount INT DEFAULT NULL,
+    SalesPrice DECIMAL(20,2) DEFAULT NULL,
+    FOREIGN KEY (SalesOrderID) REFERENCES SalesOrder (SalesOrderID),
+    FOREIGN KEY (ProductID) REFERENCES Product (ProductID),
+    PRIMARY KEY(SalesOrderID, ProductID)
+) 
 
-CREATE TABLE `TimeStamps` (
-  `EmployeeID` char(6) NOT NULL,
-  `WorkDate` date DEFAULT NULL,
-  `WorkHours` int(2) DEFAULT NULL,
-  `WorkStatus` varchar(10) DEFAULT NULL,
-  `Notice` varchar(30) DEFAULT NULL,
-  KEY `EmployeeID` (`EmployeeID`),
-  CONSTRAINT `TimeStamps_ibfk_1` FOREIGN KEY (`EmployeeID`) REFERENCES `Employee` (`EmployeeID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE Supplier (
+    SupplierID char(6) NOT NULL,
+    SupplierName varchar(25) NOT NULL,
+    ContactPerson varchar(25) NOT NULL,
+    Address varchar(50) DEFAULT NULL,
+    PostalCode varchar(4) DEFAULT NULL,
+    City varchar(25) DEFAULT NULL,
+    Phone varchar(8) NOT NULL,
+    PRIMARY KEY (SupplierID)
+) 
+
+CREATE TABLE TimeStamps (
+    EmployeeID char(6) NOT NULL,
+    WorkDate date NOT NULL,
+    WorkHours INT NOT NULL,
+    WorkStatus varchar(10) DEFAULT NULL,
+    Notice varchar(30) DEFAULT NULL,
+    BossID char(6) DEFAULT NULL,
+    FOREIGN KEY (EmployeeID) REFERENCES Employee (EmployeeID),
+    FOREIGN KEY (BossID) REFERENCES Employee (EmployeeID),
+    PRIMARY KEY (EmployeeID, WorkDate)
+) 
