6acae3bc3e9c28ce66bfbff506e2055d4f1b2b70
Oliver Comprés Poulsen <oli@live.dk>
2020-04-16 15:41:36 +0200

    Update to deparment views

--- SQL_Strings/SalesDepartmentViews.sql
+++ SQL_Strings/SalesDepartmentViews.sql
@@ -7,7 +7,7 @@ INNER JOIN Customer ON SalesOrder.CustomerID = Customer.CustomerID;
 SELECT * FROM Dispatch;
 
 CREATE VIEW Invoice
-AS SELECT packing_list.SalesOrderID, packing_list.ProductID, packing_list.Details, packing_list.Amount, Product.SalesPrice FROM packing_list
+AS SELECT packing_list.SalesOrderID, packing_list.ProductID, packing_list.Details, packing_list.Amount, Product.SalesPrice, packing_list.Amount*Product.SalesPrice 'TotalLinePrice' FROM packing_list
 INNER JOIN Product ON packing_list.ProductID = Product.ProductID;
 
 SELECT * FROM Invoice;
