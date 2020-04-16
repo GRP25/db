3fe0ddd36201645e35c37dc803ef4638dd823ed2
Martin <mail@mama.sh>
2020-04-16 17:35:38 +0200

    Test data under delvmnt

---
+++ SQL_Strings/TestData/departments.sql
@@ -0,0 +1,3 @@
+INSERT INTO Department (DepartmentName) VALUES ('Sales');
+INSERT INTO Department (DepartmentName) VALUES ('Purchases');
+INSERT INTO Department (DepartmentName) VALUES ('Admin');
---
+++ SQL_Strings/TestData/employees.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Malte','Petersen','Admin',20200413,1234567890);
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Lise','Jacobsen','Admin',20200413,1234567890);
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Anders','Bentsen','Admin',20200413,1234567890);
+
+
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Christine','Dahl','Sale',20200413,1234567890); 
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Hans','Christensen','Sale',20200413,1234567890); 
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Ole','Hansen','Sale',20200413,1234567890); 
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Iben','Leth','Sale',20200413,1234567890); 
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Hans Peter','Mortensen','Sale',20200413,1234567890); 
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Leif','Jensen','Sale',20200413,1234567890); 
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Morten','Petersen','Sale',20200413,1234567890); 
+
+
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Eva Anette','Frederiksen','Purchase',20200413,1234567890); 
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Viggo','Alberg','Purchase',20200413,1234567890); 
+INSERT INTO `Employee` (`FirstName`, `LastName`,`Department`,`StartDate`,`AccountNo`) VALUES ('Ea','Fluvholm','Purchase',20200413,1234567890); 
