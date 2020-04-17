1b527dca20390ac774dbf67cad2d8824af947404
Thomas <thomas8@hohnen.dk>
2020-04-17 19:34:01 +0200

    Made two methods for the sales class

--- src/main/java/Sales.java
+++ src/main/java/Sales.java
@@ -1,5 +1,15 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Sales{
+
+    Connection conn;
+
+    public Sales(Connection conn) {
+        this.conn = conn;
+    }
 
     public void addCustomer(){
 
@@ -21,11 +31,40 @@ public class Sales{
 
     }
 
-    public void packingList(String productID){
-        String sql = "SELECT * FROM packing_list WHERE ProductID = ?";
+    public void packingList(String SalesOrderID) throws SQLException {
+
+        String sql = "SELECT * FROM packing_list WHERE SalesOrderID = ?";
+        PreparedStatement pstm = conn.prepareStatement(sql);
+        pstm.setString(1,SalesOrderID);
+        ResultSet rs = pstm.executeQuery();
+
+        while(rs.next()) {
+
+            String sID = rs.getString("SalesOrderID");
+            String pID = rs.getString("ProductID");
+            String detail = rs.getString("Details");
+            String amout = rs.getString("Amount");
+
+            System.out.format("%s, %s, %s, %s\n", sID , pID, detail, amout);
+        }
     }
 
-    public void invoice(){
+    public void invoice(String SalesOrderID) throws SQLException {
+
+        String sql = "CALL sendOrder(?) ";
+        PreparedStatement pstm = conn.prepareStatement(sql);
+        pstm.setString(1,SalesOrderID);
+        ResultSet rs = pstm.executeQuery();
+
+        while(rs.next()){
+            String pID = rs.getString("ProductID");
+            String details = rs.getString("Details");
+            String amount = rs.getString("Amount");
+            String saleP = rs.getString("SalesPrice");
+            String linePrice = rs.getString("TotalLinePrice");
+
+            System.out.format("%s, %s, %s, %s, %s\n",pID,details,amount,saleP,linePrice);
+        }
 
     }
 
