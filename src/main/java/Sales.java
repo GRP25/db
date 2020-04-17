cd5166fbbbb5894cec1109a5230fc82b0368582d
Thomas <thomas8@hohnen.dk>
2020-04-17 19:03:30 +0200

    began work on sales class in java

---
+++ src/main/java/Sales.java
@@ -0,0 +1,40 @@
+
+public class Sales{
+
+    public void addCustomer(){
+
+    }
+
+    public void addSale(){
+
+    }
+
+    public void adminCustomer(){
+
+    }
+
+    public void adminSale(){
+
+    }
+
+    public void addProductToSale(){
+
+    }
+
+    public void packingList(String productID){
+        String sql = "SELECT * FROM packing_list WHERE ProductID = ?";
+    }
+
+    public void invoice(){
+
+    }
+
+    public void dispatch(){
+
+    }
+
+    public void paymentReg(){
+
+    }
+
+}
