c1e1523d6175794d148e10c24f68abc918e96071
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-04-20 17:55:48 +0200

    Purchases class is work[Test is done]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import SQL.DatabaseCtrl;
 import SQL.Login;
+import SQL.Purchases;
+
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,7 +11,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Input employee ID to login:");
-        String userID = "M00005"; //Dette er Malte
+        String userID = "M00012"; //Dette er Malte = M00005
         Login l = new Login();
         String[] returns = l.login(userID);
         System.out.println(returns);
@@ -36,10 +38,13 @@ public class Main {
                 }
                 break;
             case "Purchases":
-                if (returns[1].equals("Fulltime") || returns.equals("Parttime"))
+                if (returns[1].equals("Fulltime") || returns.equals("Parttime")){
                     conn = SQL.DatabaseCtrl.connect("Purchases","1234");
-                else if (returns[1].equals("Boss"))
+                        new Purchases(conn);
+                } else if (returns[1].equals("Boss")){
                     conn = SQL.DatabaseCtrl.connect("CPO","1234");
+                    new Purchases(conn);
+                }
                 break;
             default:
                 break;
